e971e207699b3dfaa7b1468700147c7f86efd528
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-19 08:41:55 +0300

    First commit where all tests are green

--- src/main/kotlin/ArmstrongNumber.kt
+++ src/main/kotlin/ArmstrongNumber.kt
@@ -1,7 +1,13 @@
+import kotlin.math.pow
+
 object ArmstrongNumber {
 
     fun check(input: Int): Boolean {
-        TODO("Implement the function to complete the task")
+        val iString = input.toString()
+        val numberOfDigits = iString.length
+        return iString.fold(0.toDouble()) { acc, d ->
+            acc + d.toString().toDouble().pow(numberOfDigits.toDouble())
+        } == input.toDouble()
     }
 
 }
